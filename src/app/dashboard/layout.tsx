d67935e9bf3c109bb21f7ac5704d13ca4c25ee4c
srajan <kumarsrajan02@gmail.com>
2024-09-09 22:24:30 +0530

    add: Suspense

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,13 +2,16 @@
 
 import Header from "@/components/shared/header";
 import Sidebar from "@/components/shared/sidebar";
+import { Suspense } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex">
       <Sidebar />
       <Header />
-      <div className="pl-20 pt-20 pr-4 w-full">{children}</div>
+      <div className="pl-20 pt-20 pr-4 w-full">
+        <Suspense>{children}</Suspense>
+      </div>
     </div>
   );
 }
